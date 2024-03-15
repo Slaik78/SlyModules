629eca44a2e4dfa1cb12de0aa133dc636174eb8a
Slaik <154437106+Slaik78@users.noreply.github.com>
2024-03-15 21:12:02 +0300

    Add files via upload

---
+++ PetPet.py
@@ -0,0 +1,32 @@
+import os
+from hikkatl.types import Message
+from .. import loader, utils
+from io import BytesIO
+from petpetgif import petpet
+
+#meta-developer:@Hicota
+
+@loader.tds
+class PetPetMod(loader.Module):
+    """Развлекательный модуль"""
+    
+    strings = {"name": "PetPet"}
+
+    @loader.command()
+    async def pet(self, message: Message):
+        """гладит фото"""
+        response = await message.get_reply_message()
+    
+        try:
+            media = await self._client.download_media(response.photo, "pet")
+            petgif = BytesIO()
+            petpet.make(media, petgif)
+            petgif.name = "pet.gif"
+            petgif.seek(0)
+        except AttributeError :
+            await utils.answer(message, '<emoji document_id=5465665476971471368>❌</emoji><b>Это не фото</b><emoji document_id=5465665476971471368>❌</emoji>\nОтветьте на фото чтобы команда .pet заработала')
+            return
+
+        await self._client.send_file(message.to_id, file=petgif, force_document=False)
+        await message.delete()
+        os.remove(media)
