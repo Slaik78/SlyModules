f7bff07b8568790a6c53f27dfd5f8692dfde2361
Slaik <154437106+Slaik78@users.noreply.github.com>
2025-04-21 21:29:26 +0300

    Add files via upload

--- 1.6.3 and 1.6.8/heroku_info.py
+++ 1.6.3 and 1.6.8/heroku_info.py
@@ -77,7 +77,7 @@ class HerokuInfoMod(loader.Module):
         text = f"{re.sub(reg, '', text)}"
         allchars = [str for str in text]
         emoji_list = [c for c in allchars if c in emoji.EMOJI_DATA]
-        clean_text = ' '.join([str for str in text if not any(i in str for i in emoji_list)])
+        clean_text = ''.join([str for str in text if not any(i in str for i in emoji_list)])
         return clean_text
     
     def imgurpidor(self, url: str) -> str:
